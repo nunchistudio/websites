f760aeadbf7909d1d10d39e6b82f0e8071e7da96
Loïc Saint-Roch <loic@nunchi.studio>
2023-07-13 11:22:24 +0200

    fix(next): Add redirection from /blacksmith to /helix
    Signed-off-by: Loïc Saint-Roch <loic@nunchi.studio>

--- domains/nunchi.studio/next.config.js
+++ domains/nunchi.studio/next.config.js
@@ -3,4 +3,14 @@ const withTranspile = require('next-transpile-modules')([
   '@nunchistudio/base',
 ]);
 
-module.exports = withTranspile(NextConfig);
+module.exports = withTranspile(Object.assign(NextConfig, {
+  async redirects() {
+    return [
+      {
+        source: '/blacksmith/:path*',
+        destination: '/helix',
+        permanent: true,
+      },
+    ]
+  },
+}));
