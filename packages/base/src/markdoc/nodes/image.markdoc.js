5e863325d755eeef81ffc9240f30e15b98913d56
Loïc Saint-Roch <loic@nunchi.studio>
2023-11-30 07:16:02 +0100

    fix(markdoc): Remove hydration issue in image nodes
    Signed-off-by: Loïc Saint-Roch <loic@nunchi.studio>

--- packages/base/src/markdoc/nodes/image.markdoc.js
+++ packages/base/src/markdoc/nodes/image.markdoc.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { nodes } from '@markdoc/markdoc';
 
-import { EuiImage, EuiSpacer } from '@elastic/eui'
-
 import { useTheme } from '../../components/Theme';
 
 const Image = (props) => {
   const { colorMode } = useTheme();
   const src = props.src.replace('%7Btheme%7D', colorMode);
 
-  return (
-    <>
-      <EuiSpacer size="l" />
-      <EuiImage src={src} alt={props.alt} />
-    </>
-  );
+  return <img src={src} alt={props.alt} />;
 };
 
 export default {
